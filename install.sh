eaba333ec1b8a3591570dfe3452dd6ed7959d283
wakemaster39 <wakemaster39@users.noreply.github.com>
2018-08-28 14:41:58 -0400

    Update install.sh
    remove cloning tmux-config

--- install.sh
+++ install.sh
@@ -92,7 +92,6 @@ ln -sf "$DOTFILES"/ssh_config "$HOME"/.ssh/config
 
 
 # tmux-config
-git clone https://github.com/samoshkin/tmux-config.git "${DOTFILES}/tmux-config"
 "${DOTFILES}/tmux-config/install.sh"
 
 # pipenv typically is already installed
